9e6e08e1305992ee82d5e09a69e84a5fb9287e7c
SimonRO06 <simon.rubiano.ortiz@gmail.com>
2025-10-06 22:46:37 -0500

    feat: :sparkles: Add AppDbContext

--- Domain/Entities/Vehiculo.cs
+++ Domain/Entities/Vehiculo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Domain.Entities;
 
-namespace Domain.Entidades;
+namespace Domain.Entities;
 
 public class Vehiculo
 {
---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Cliente> Clientes => Set<Cliente>();
+    public DbSet<DetalleOrden> DetallesOrdenes => Set<DetalleOrden>();
+    public DbSet<Factura> Facturas => Set<Factura>();
+    public DbSet<Marca> Marcas => Set<Marca>();
+    public DbSet<OrdenServicio> OrdenesServicios => Set<OrdenServicio>();
+    public DbSet<Pago> Pagos => Set<Pago>();
+    public DbSet<Repuesto> Repuestos => Set<Repuesto>();
+    public DbSet<Usuario> Usuarios => Set<Usuario>();
+    public DbSet<Vehiculo> Vehiculos => Set<Vehiculo>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+}
