2fcf7fb986be8436b2bef3c980a40bd38b97d55e
SimonRO06 <simon.rubiano.ortiz@gmail.com>
2025-10-06 23:35:27 -0500

    feat: :sparkles: Add IUnitOfWork and UnitOfWork content

--- Application/Abstractions/IUnitOfWork.cs
+++ Application/Abstractions/IUnitOfWork.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 namespace Application.Abstractions;
 public interface IUnitOfWork
 {
-    // IProductRepository Products { get; }
+    IClienteRepository Clientes { get; }
+    IDetalleOrdenRepository DetallesOrdenes { get; }
+    IFacturaRepository Facturas { get; }
+    IMarcaRepository Marcas { get; }
+    IOrdenServicioRepository OrdenesServicios { get; }
+    IPagoRepository Pagos { get; }
+    IRepuestoRepository Repuestos { get; }
+    IUsuarioRepository Usuarios { get; }
+    IVehiculoRepository Vehiculos { get; }
     // Task<int> SaveAsync();
     Task<int> SaveChanges(CancellationToken ct = default);
     Task ExecuteInTransactionAsync(Func<CancellationToken, Task> operation, CancellationToken ct = default);
--- Infrastructure/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -2,9 +2,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Abstractions;
+using Infrastructure.Persistence;
 
 namespace Infrastructure.UnitOfWork;
-public class UnitOfWork
+
+public class UnitOfWork : IUnitOfWork
 {
-    
+    private readonly AppDbContext _context;
+    private IClienteRepository? _clienteRepository;
+    private IDetalleOrdenRepository? _detalleOrdenRepository;
+    private IFacturaRepository? _facturaRepository;
+    private IMarcaRepository? _marcaRepository;
+    private IOrdenServicioRepository? _ordenServicioRepository;
+    private IPagoRepository? _pagoRepository;
+    private IRepuestoRepository? _repuestoRepository;
+    private IUsuarioRepository? _usuarioRepository;
+    private IVehiculoRepository? _vehiculoRepository;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+    public Task<int> SaveChanges(CancellationToken ct = default)
+        => _context.SaveChangesAsync(ct);
+    public async Task ExecuteInTransactionAsync(Func<CancellationToken, Task> operation, CancellationToken ct = default)
+    {
+        await using var tx = await _context.Database.BeginTransactionAsync(ct);
+        try
+        {
+            await operation(ct);
+            await _context.SaveChangesAsync(ct);
+            await tx.CommitAsync(ct);
+        }
+        catch
+        {
+            await tx.RollbackAsync(ct);
+            throw;
+        }
+    }
+    public IClienteRepository Clientes => _clienteRepository ??= new ClienteRepository(_context);
+    public IDetalleOrdenRepository DetallesOrdenes => _detalleOrdenRepository ??= new DetalleOrdenRepository(_context);
+    public IFacturaRepository Facturas => _facturaRepository ??= new FacturaRepository(_context);
+    public IMarcaRepository Marcas => _marcaRepository ??= new MarcaRepository(_context);
+    public IOrdenServicioRepository OrdenesServicios => _ordenServicioRepository ??= new OrdenServicioRepository(_context);
+    public IPagoRepository Pagos => _pagoRepository ??= new PagoRepository(_context);
+    public IRepuestoRepository Repuestos => _repuestoRepository ??= new RepuestoRepository(_context);
+    public IUsuarioRepository Usuarios => _usuarioRepository ??= new UsuarioRepository(_context);
+    public IVehiculoRepository Vehiculos => _vehiculoRepository ??= new VehiculoRepository(_context);
+    public IUserMemberRolService UserMemberRoles => throw new NotImplementedException();
 }
